0c69a6e1a41771b1f64ac89c3c3c97770df376f1
Michael El Omar <michael.elomar@parrot.com>
2023-10-07 12:58:01 +0200

    Add Brands division on the website

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const WhatGPT3 = () => (
+    <div>
+        <h1>WhatGPT3</h1>
+    </div>
+);
+
+export default WhatGPT3;
--- src/containers/whatGPT4/WhatGPT4.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const WhatGPT4 = () => (
-    <div>
-        <h1>WhatGPT4</h1>
-    </div>
-);
-
-export default WhatGPT4;
