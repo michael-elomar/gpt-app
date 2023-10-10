068b76d92a7dafb80df6adc81d8f0946156ad4c3
Michael El Omar <michael.elomar@parrot.com>
2023-10-10 23:31:58 +0200

    add features and possibility view

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-
+import PossibilityImg from "../../assets/possibility.png"
+import "./possibility.css"
 const Possiblity = () => (
-    <div>
-        <h1>Possiblity</h1>
+    <div className="gpt3__possibility section__margin">
+        <div className="gpt3__possibility-img">
+            <img src={PossibilityImg} />
+        </div>
+        <div className="gpt3__possibility-content">
+            <h2>Request Early Access to Get Started</h2>
+            <h1>The possibilities are beyond your imagination</h1>
+            <p>
+                Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing.
+                Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+            </p>
+            <h3>Request Early Access to Get Started</h3>
+        </div>
     </div>
 );
 
