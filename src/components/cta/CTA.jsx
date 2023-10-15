1ca7b86323bfefe620b83f71897580e066626b1f
Michael El Omar <quisutdeus100@gmail.com>
2023-10-15 10:57:43 +0200

    Add CTA section and style it

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import "./cta.css";
 
 const CTA = () => (
-    <div>
-        <h1>CTA</h1>
+    <div className="gpt3__cta section__margin">
+        <div className="gpt3__cta-content">
+            <h5>Request Early Access to Get Started</h5>
+            <h1>Register today & start exploring the endless possiblities.</h1>
+        </div>
+        <div className="gpt3__cta-button">
+            <button type="button">Get Started</button>
+        </div>
     </div>
 );
 
